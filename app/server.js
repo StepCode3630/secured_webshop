44d71ab47e512e65a0c4c5ad50158a7b6852f2f1
Stepan <stepanpatricny@gmail.com>
2026-04-29 10:00:36 +0200

    fix: type ESmodule
     [10] [Done]

--- app/server.js
+++ app/server.js
@@ -1,13 +1,13 @@
-import dotenv from "dotenv";
+import "dotenv/config.js";
 import path from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config({
-  path: path.resolve(__dirname, "../.env"),
-});
+// dotenv.config({
+//   path: path.resolve(__dirname, "../.env"),
+// });
 
 import cookieParser from "cookie-parser";
 import express from "express";
